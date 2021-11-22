e29def33ea7235cc70663734740e700dd8c88f6d
ValetCona <receive@vcona.net>
2021-11-22 11:00:57 +0900

    #5 コピーボタンを追加

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,22 @@ import {
   List,
   ListItem,
   ListItemSecondaryAction,
-  ListItemIcon
+  ListItemIcon,
+  Snackbar
 } from '@mui/material';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import { Container, Draggable, DropResult } from "react-smooth-dnd";
 import { arrayMoveImmutable } from "array-move";
+import CopyToClipBoard from 'react-copy-to-clipboard';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function App() {
   const defaultColors = ['#C7000B', '#D28300', '#DFD000', '#00873C', '#005AA0', '#181878', '#800072'];
@@ -34,6 +44,7 @@ function App() {
   const [isCheckedColor, setIsCheckedColor] = useState(false);
   const [preText, setPreText] = useState("");
   const [resultText, setResultText] = useState("");
+  const [open, setOpen] = useState(false);
 
   const onDrop = (dropResult: DropResult) => {
     const { removedIndex, addedIndex } = dropResult;
@@ -69,6 +80,18 @@ function App() {
     
     setResultText(result);
   }
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <div className="App">
@@ -133,7 +156,15 @@ function App() {
       </Card>
 
       <Box sx={{ m: 2 }}>
-        <Button variant="contained" onClick={onClickButton}>生成</Button>
+        <Button variant="contained" onClick={onClickButton}>虹色にする</Button>
+        <CopyToClipBoard text={resultText}>
+          <Button variant="contained" onClick={handleClick} sx={{ml: 2}}>結果をコピー</Button>
+        </CopyToClipBoard>
+        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            クリップボードにコピーしました
+          </Alert>
+        </Snackbar>
       </Box>
 
       <Card sx={{ m: 2 }}>
